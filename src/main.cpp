28ed43f17d5a6953aadce6a0267bddc98f84799e
Salva Romero <salvaromero08@gmail.com>
2023-05-09 22:37:17 +0200

    fuzzy metals

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,7 @@ int main() {
     const int image_width = 300;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     std::vector<color> image(image_width * image_height);
-    const int samples_per_pixel = 100;
+    const int samples_per_pixel = 200;
     const int max_depth = 50;
     // Camera
     camera cam;
@@ -42,15 +42,15 @@ int main() {
     // World
     hittable_list world;
 
-    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8));
-    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2));
+    auto material_ground = make_shared<lambertian>(color(0.1, 0.1, 0.1));
+    auto material_left = make_shared<lambertian>(color(120.0 / 255.0, 254.0 / 255.0, 207.0 / 255.0));
+    auto material_right = make_shared<lambertian>(color(191.0 / 255.0, 78.0 / 255.0, 48 / 255.0));
+    auto material_center = make_shared<metal>(color(198.0 / 255.0, 204.0 / 255.0, 178.0 / 255.0), 0.2);
 
     world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
-    world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
-    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
-    world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
+    world.add(make_shared<sphere>(point3(0.0, 1.5, -3.0), 2, material_center));
+    world.add(make_shared<sphere>(point3(-0.5, 0.0, -1.0), 0.5, material_left));
+    world.add(make_shared<sphere>(point3(0.5, 0.0, -1.0), 0.5, material_right));
 
     // Render
     auto start = high_resolution_clock::now();
