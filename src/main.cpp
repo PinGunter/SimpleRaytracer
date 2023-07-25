6d4a6b3b7e0978c311fa7a58ce9d5dc2e23e294e
Salva Romero <salvaromero08@gmail.com>
2023-07-25 19:30:03 +0200

    camera fov and lookfrom/at

--- src/main.cpp
+++ src/main.cpp
@@ -36,21 +36,20 @@ int main() {
     const int samples_per_pixel = 200;
     const int max_depth = 50;
     // Camera
-    camera cam;
-
-
-    // World
     hittable_list world;
 
-    auto material_ground = make_shared<lambertian>(color(0.1, 0.1, 0.1));
+    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
     auto material_left = make_shared<dielectric>(1.5);
-    auto material_right = make_shared<lambertian>(color(191.0 / 255.0, 78.0 / 255.0, 48 / 255.0));
-    auto material_center = make_shared<metal>(color(198.0 / 255.0, 204.0 / 255.0, 178.0 / 255.0), 0.2);
+    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
     world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
-//    world.add(make_shared<sphere>(point3(0.0, 1.5, -3.0), 2, material_center));
-    world.add(make_shared<sphere>(point3(-0.5, 0.0, -1.0), 0.5, material_left));
-    world.add(make_shared<sphere>(point3(0.5, 0.0, -1.0), 0.5, material_right));
+    world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.45, material_left));
+    world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
+
+    camera cam(point3(-2, 2, 1), point3(0, 0, -1), vec3(0, 1, 0), 90, aspect_ratio);
 
     // Render
     auto start = high_resolution_clock::now();
